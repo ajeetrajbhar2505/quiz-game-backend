9a3aa7fdd59373c5a74249d8587180e8ad390f55
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-03-18 23:02:13 +0530

    wallet schema updated

--- models/wallet.js
+++ models/wallet.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const WalletSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Linked to User
   balance: { type: Number, default: 0 }, // Wallet balance in INR
   createdAt: { type: Date, default: Date.now }
 });
