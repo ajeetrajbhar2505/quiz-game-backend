fcc392c62e249676cec81fbe5d33b1d41a3d7aaf
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-03-18 23:28:36 +0530

    added profilePicture in user schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 // Define the User Schema
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  profilePicture : { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phoneNumber: { type: String, unique: true, sparse: true }, // Optional for OTP
   password: { type: String, required: true },
