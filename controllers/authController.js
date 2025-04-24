2315eb50e7f8d5825140a6a00ee91ec88ba2d36f
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-04-25 00:34:14 +0530

    https

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,7 @@ const bcrypt = require('bcryptjs');
 const { getIO } = require('./socketController');
 require('dotenv').config();
 const querystring = require('querystring');
+const https = require('https');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL);
 
 
